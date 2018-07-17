608812751a1d69f2f37d91c3073736e419d0979a
sboisseau <sebastien.boisseau@gmail.com>
2018-07-17 08:54:07 +0200

    Main - maj label

--- src/main/java/org/sbu/test/HelloWorld.java
+++ src/main/java/org/sbu/test/HelloWorld.java
@@ -2,6 +2,6 @@ package org.sbu.test;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello Master");
     }
 }
