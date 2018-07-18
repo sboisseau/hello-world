9f5e841d48a9901ddb701afb00b2f8240713d8ef
sboisseau <sebastien.boisseau@gmail.com>
2018-07-18 13:45:59 +0200

    maj helloWorld from br2

--- src/main/java/org/sbu/test/HelloWorld.java
+++ src/main/java/org/sbu/test/HelloWorld.java
@@ -3,5 +3,6 @@ package org.sbu.test;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Master");
+        System.out.println("Hello Master br2");
     }
 }
